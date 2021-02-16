7a14c050994702cdf61261d4079b1b61cf8f5be3
Tanzim Siddiqee <tanzimsiddiqee@hotmail.com>
2021-02-16 16:13:16 +0600

    ForLoop using console read

---
+++ ForLoop/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ForLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter loop endpoint: ");
+            int j = int.Parse(Console.ReadLine());
+
+            for(int i = 1; i <= j; i++)
+            {
+                Console.WriteLine("Value of i: {0} of {1}", i, j);
+            }
+        }
+    }
+}
