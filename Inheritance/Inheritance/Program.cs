3e577834af60894f9f536ba0d0ec2399c70a7aab
Tanzim Siddiqee <tanzimsiddiqee@hotmail.com>
2021-02-21 17:14:11 +0600

    Interface class added

---
+++ Inheritance/Inheritance/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Inheritance
+{
+    class A
+    {
+        List<int> list = new List<int>();
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
